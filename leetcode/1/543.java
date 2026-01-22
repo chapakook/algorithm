7d1e2b0b32558d70f47ac8521820eb82bb98dae7
chapakook <chapakook96@gmail.com>
2026-01-22 22:15:21 +0900

    feat: add solved 543.java

---
+++ leetcode/1/543.java
@@ -0,0 +1,36 @@
+class Solution {
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  private int diameter = 0;
+
+  public int diameterOfBinaryTree(TreeNode root) {
+    height(root);
+    return diameter;
+  }
+
+  private int height(TreeNode node) {
+    if (node == null)
+      return 0;
+    int left = height(node.left);
+    int right = height(node.right);
+    diameter = Math.max(diameter, left + right);
+    return Math.max(left, right) + 1;
+  }
+}
